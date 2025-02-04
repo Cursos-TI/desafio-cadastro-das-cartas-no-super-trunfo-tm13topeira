47d5fa9cf6641e07523a5562da3cce4406649fda
tm13topeira <tm13.topeira@gmail.com>
2025-02-04 20:38:02 +0000

    pintf e scanf adicionados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Uma carta
 
 int main() {
     char estado; // de A-H
     char carta; // de 1 a 4
     char nome; // nome da cidade
-    int Populacao;
+    int populacao;
     float area; // km²
-    float PIB;
+    float pib;
     int ponto;  // número de pontos turísticos
     
+    printf("Digite o nome da cidade: \n");
+    scanf(" %s", &nome);
 
+    printf("Digite a população: \n");
+    scanf(" %d", &populacao);
+
+    printf("Digite a área em km²: \n");
+    scanf(" %f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf(" %f", &pib);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf(" %d", &ponto);
 
     return 0;
 }
